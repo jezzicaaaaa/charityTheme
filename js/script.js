56dac98b3a2d4b894e896a0d6481b2848867bd3e
Jezzica <jezzicamae@gmail.com>
2017-11-25 13:11:17 -0500

    bug fixed thru jQuery

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-$( document ).ready(function() {
-    $( ".cross" ).hide();
-    $( ".menu" ).hide();
-    $( ".hamburger" ).click(function() {
-        $( ".hamburger" ).hide();
-        $( ".menu" ).slideToggle( "fast", function() {
-            $( ".cross" ).show();
-        });
+$(document).ready(function() {
+  $(".cross").hide();
+  $(".menu").hide();
+  $(".hamburger").click(function() {
+    $(".menu").slideToggle("fast", function() {
+      $(".hamburger").hide();
+      $(".cross").show();
     });
-    $( ".cross" ).click(function() {
-        $( ".cross" ).hide();
-        $( ".menu" ).slideToggle( "fast", function() {
-            $( ".hamburger" ).show();
-        });
+  });
+  $(".cross").click(function() {
+    $(".menu").slideToggle("fast", function() {
+      $(".cross").hide();
+      $(".hamburger").show();
     });
+  });
+  $(window).on("resize", function() {
+    if (window.innerWidth > 767) {
+      $(".hamburger").hide();
+    } else {
+      $(".hamburger").show();
+    }
+  });
 });
